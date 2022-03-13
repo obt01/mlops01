0575c40c9e77a6fec091be816d9030030bea513d
obata <tech.obata1205@gmail.com>
2022-03-13 21:16:26 +0900

    TorchServe導入

--- models/output_torch_model_file.py
+++ models/output_torch_model_file.py
@@ -1,8 +1,11 @@
+import os
 import torch
 from torchvision import models  # type: ignore
 
+model_name = os.environ["MODEL_NAME"]
 
-def load_model(model_name: str = "resnet101") -> models:
+
+def load_model(model_name: str = model_name) -> models:
     model = getattr(models, model_name)(pretrained=True)
     return model
 
@@ -11,7 +14,7 @@ def save_model(model: models) -> None:
     model.eval()
     example_input = torch.rand(1, 3, 224, 224)
     traced_script_module = torch.jit.trace(model, example_input)
-    traced_script_module.save("resnet-101.pt")
+    traced_script_module.save(f"{model_name}.pt")
 
 
 if __name__ == "__main__":
--- src/app/routers.py
+++ src/app/routers.py
@@ -1,9 +1,20 @@
 from __future__ import annotations
+import requests
+import json
 from fastapi import APIRouter
-from src.ml.prediction import classifier
+import urllib.request
+import urllib.parse
+# from src.ml.prediction import classifier
 
 router = APIRouter()
 
+
+# data = urllib.parse.urlencode({'foo': 'bar'}).encode('utf-8')
+# request = urllib.request.Request('http://www.example.com', data)
+# response = urllib.request.urlopen(request)
+# print(response.getcode())
+# html = response.read()
+# print(html.decode('utf-8'))
 
 @router.get("/health")
 def health() -> dict[str, str]:
@@ -13,11 +24,27 @@ def health() -> dict[str, str]:
     return {"health": "ok"}
 
 
+@router.get("/health_torchserve")
+def health_torchserve() -> dict[str, str]:
+    """
+    ヘルスチェック
+    """
+    res = urllib.request.urlopen("http://localhost:8080/ping")
+    # res = urllib.request.urlopen("https://google.com")
+    # res = requests.get("http://host.docker.internal:8080/ping")
+    # res = requests.get("https://google.com")
+    return {"health": res}
+
+
 @router.get("/predict/test")
 def predict_test() -> dict[str, float]:
     """
     推論テスト
     """
-    img_path = "/app/data/sample/bobby.jpg"
-    pred = classifier.predict(img_path)
-    return pred
+    img_path = "/app/data/bobby.jpg"
+    url = "http://host.docker.internal:8080/predictions/resnet101"
+    with open(img_path, "rb") as img:
+        data = json.dumps({"data": img})
+        # print(vars(img))
+        res = requests.post(url, files=data)
+    return res
